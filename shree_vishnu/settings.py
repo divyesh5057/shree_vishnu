c0c00a1f27565294388c93e19bb2bf915ff4c516
divyesh5057 <divyesh.patil.5057@gmail.com>
2023-05-08 12:57:55 +0530

    vercel_change

--- shree_vishnu/settings.py
+++ shree_vishnu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6)py!p(r_i+fbgvkczic6#jv62(6dziijisy3mb&1ehyal48f(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.37','*']
+ALLOWED_HOSTS = ['192.168.1.37','*','127.0.0.1', '.vercel.app']
 
 
 # Application definition
